d078f692b01d4670c94632352a464f9c6f8a6e25
megahomyak <g.megahomyak@gmail.com>
2025-02-08 02:09:40 +0500

    Making this overly complicated for a good UI

---
+++ program.py
@@ -0,0 +1,75 @@
+from random import randint as _randint
+import random as _random
+from types import SimpleNamespace as _SN
+
+def _randstrat(*args):
+    strat_weights = args[::2]
+    strat_population = args[1::2]
+    return _random.choices(strat_population, strat_weights)[0]
+
+def _enter_nonpunct(data, after):
+    def inner(ctx):
+        if ctx.result != "":
+            ctx.result += " "
+        _enter(data)(ctx)
+        return after
+    return inner
+
+def _enter(data, after):
+    def inner(ctx):
+        ctx.result += data
+    return inner
+
+def _new_paragraph(ctx):
+    ctx.result += "\n\n"
+
+_period = _enter_nonpunct(".")
+_comma = _enter_nonpunct(",")
+_exclamation_mark = _enter_nonpunct("!")
+_question_mark = _enter_nonpunct("?")
+
+def _period():
+    return _enter_nonpunct(".")
+
+def _comma():
+    return _enter_nonpunct(",")
+
+def _exclamation_mark():
+    return _enter_nonpunct("!")
+
+def _word(ctx):
+    pass
+
+def _question_mark(ctx):
+    pass
+
+def _number(ctx):
+
+
+def _parens(ctx):
+    _enter_nonpunct(ctx, "(")
+    for i in range(randint()):
+        _parens_randstrat(ctx)
+    ctx.result += ")"
+    ctx.space_needed = True
+
+def encode(octets: "iterable of ints"):
+    n = 0
+    for octet in octets:
+        n *= 257
+        n += octet + 1
+    letters = []
+    while n != 0:
+        quot, n = divmod(n, 26)
+        letters.append("abcdefghijklmnopqrstuvwxyz"[quot])
+    ctx = _SN(
+        letters=letters,
+        result="",
+    )
+    thunk = _beginning()
+    while True:
+        thunk = thunk(ctx)
+
+    while thunk is not None:
+        thunk = thunk()(ctx)
+    return ctx.result
